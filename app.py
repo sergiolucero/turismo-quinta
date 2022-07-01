8341d1589531dadcd98aa17f76cc9be3be1ea1b6
Sergio Lucero <sergiolucero@gmail.com>
2022-07-01 15:50:08 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+def Layer(layer_type, df):
+  return pdk.Layer(layer_type, data=df,
+            get_position='[lon, lat]',
+            radius=200, elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True, extruded=True,
+         )
+
+st.pydeck_chart(pdk.Deck(
+     map_style='mapbox://styles/mapbox/light-v9',
+     initial_view_state=pdk.ViewState(
+         latitude=37.76,
+         longitude=-122.4,
+         zoom=11,
+         pitch=50,
+     ),
+     layers=[
+         Layer('HexagonLayer',df),
+         pdk.Layer(
+             'ScatterplotLayer',
+             data=df,
+             get_position='[lon, lat]',
+             get_color='[200, 30, 0, 160]',
+             get_radius=200,
+         ),
+     ],
+ ))
