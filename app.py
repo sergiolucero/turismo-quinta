5526bd0764e5ac6a11545b56a9f81802188b1d1c
Sergio Lucero <sergiolucero@gmail.com>
2022-07-03 01:32:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ sql = lambda q: pd.read_sql(q, conn)
 ##############################################
 centro = [-33.0, -71.6] # valpo!
 df = sql('SELECT * FROM datos')
-st.write('N=%d' %len(df))
+#st.write('N=%d' %len(df))
 
 colors={'Concon': [0, 255, 128], 'San Antonio': [255, 0, 128], 'Valparaiso,Chile': [255, 128, 0],}
 layers = []
 for city, cdf in df.groupby('ciudad'):
-    st.write(city,len(cdf))
-    layers.append(Layer('HexagonLayer',df))   # write class +=
+     layers.append(Layer('HexagonLayer',df))   # write class +=
     layers.append(TextLayer(df, colors[city]))
     layers.append(ColumnLayer(df))
+st.dataframe(df.groupby('ciudad').size())
                   
-st.write('nLayers=%d' %len(layers))
+#st.write('nLayers=%d' %len(layers))
 st.pydeck_chart(pdk.Deck(
      map_style='mapbox://styles/mapbox/light-v9',
      initial_view_state=pdk.ViewState(
