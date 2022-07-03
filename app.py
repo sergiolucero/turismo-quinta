c3df5695352498ab33b78e43c6a38dd0aaec0398
Sergio Lucero <sergiolucero@gmail.com>
2022-07-03 01:20:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,28 +12,33 @@ centro = [-33.0, -71.6] # valpo!
 st.set_page_config(page_title="Turismo Quinta Región", page_icon="⬇", layout="wide")
 
 def Layer(layer_type, df):
-  return pdk.Layer(layer_type, df,
+  return pdk.Layer(layer_type, data=df,
             get_position='[lng, lat]',
-            radius=200, elevation_scale=4,
-            elevation_range=[0, 1000],
-            pickable=True, extruded=True, controller=True,
-         )
+            radius=200, elevation_scale=4, elevation_range=[0, 100],
+            pickable=True, extruded=True, controller=True)
 
 def TextLayer(df):
-    return pdk.Layer("TextLayer", df,
+    return pdk.Layer("TextLayer", data=df,
     pickable=True, get_position='[lng, lat]',   # or "coordinates",
-    get_text="name",    get_size=14,get_color=[0, 255, 0],
-    get_angle=0,
-    get_text_anchor=String("middle"),
-    get_alignment_baseline=String("center"),
-)
+    get_text="name",    get_size=16,get_color=[0, 255, 128],
+    get_angle=0, get_text_anchor=String("middle"),
+    get_alignment_baseline=String("center"))
+  
+def ColumnLayer(df):
+    return pdk.Layer("ColumnLayer", data=df,
+    get_position=["lng", "lat"],
+    get_elevation="rating",
+    elevation_scale=100,    radius=50,
+    get_fill_color=["mrt_distance * 10", "mrt_distance", "mrt_distance * 10", 140],
+    pickable=True,
+    auto_highlight=True)
 ##############################################
 
 df = sql('SELECT * FROM datos')
 st.write('N=%d' %len(df))
 
 layers = [Layer('HexagonLayer',df),  #for city, cdf in df.groupby('ciudad'),
-          TextLayer(df)],   # was IconLayer
+          TextLayer(df),ColumnLayer(df),],   # was IconLayer
 st.write('nLayers=%d' %len(layers))
 st.pydeck_chart(pdk.Deck(
      map_style='mapbox://styles/mapbox/light-v9',
