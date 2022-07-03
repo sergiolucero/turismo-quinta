c65fe314fa12db748d6e796f8fcb66ed95586b36
Sergio Lucero <sergiolucero@gmail.com>
2022-07-03 03:19:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ import numpy as np
 import pandas as pd
 from auxdeck import *
 
-import sqlite3
-conn = sqlite3.connect('turismo.db')
-sql = lambda q: pd.read_sql(q, conn)
-##############################################
-df = sql('SELECT * FROM datos')
+data = datos_turisticos()
 st.header('Elementos Turísticos Quinta Región')
-st.pydeck_chart(LayeredDeck(HexTextLayers(df)))
-st.dataframe(df.groupby('ciudad').size())
-st.dataframe(df)
+st.pydeck_chart(LayeredDeck(HexTextLayers(data)))
+st.dataframe(data.groupby('ciudad').size())
+st.dataframe(data)
