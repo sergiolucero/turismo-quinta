c6efa75cfefc9b4ba46404222fbd2f591644dbfb
Sergio Lucero <sergiolucero@gmail.com>
2022-07-04 11:58:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ from auxdeck import *
 data = datos_turisticos()
 st.header('Elementos Turísticos Quinta Región')
 st.pydeck_chart(LayeredDeck(HexTextLayers(data)))
-st.dataframe(data.groupby('ciudad').size())
+#st.dataframe(data.groupby('ciudad').size())
+st.dataframe(data.pivot_table(index='ciudad',columns='rubro'))
 st.dataframe(data)
