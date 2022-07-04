e8fffd210b7a9d2169e93c8334f048b6ec67ccc5
Sergio Lucero <sergiolucero@gmail.com>
2022-07-04 19:41:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ data = datos_turisticos()
 st.header('Elementos Turísticos Quinta Región')
 st.pydeck_chart(LayeredDeck(HexTextLayers(data)))
 st.header('Estadísticas')
-st.dataframe(data.pivot_table(index='ciudad',columns='rubro',aggfunc='count',values='lat').fillna(0))
+
+pt = data.pivot_table(index='ciudad',columns='rubro',aggfunc='count',values='lat').fillna(0)
+fig = plt.figure(figsize(10,6))
+sns.heatmap(pt, annot=True)
+st.pyplot(fig)
+#st.dataframe(pt)
+
 st.header('Datos'); st.dataframe(data)
