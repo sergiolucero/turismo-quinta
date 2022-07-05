5455527fcd4a6a3729a699122b189c5f0030e869
Sergio Lucero <sergiolucero@gmail.com>
2022-07-04 20:47:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,12 @@ from auxdeck import *
 import matplotlib.pyplot as plt
 
 data = datos_turisticos()
+
 st.header('Elementos Turísticos Quinta Región')
 st.pydeck_chart(LayeredDeck(HexTextLayers(data)))
+
 st.header('Estadísticas')
+Heatmap(data)
 
-pt = data.pivot_table(index='ciudad', columns='rubro', 
-                      values='rating', aggfunc='mean').fillna(0)
-fig = plt.figure(figsize=(5,3))
-sns.heatmap(pt, annot=True, fmt='.2f', cmap='RdYlGn',
-            annot_kws={'size':16, 'weight': 'bold'})
-st.pyplot(fig)
-#st.dataframe(pt)
-
-st.header('Datos'); st.dataframe(data)
+st.header('Datos')
+st.dataframe(data)
