5d76342b96c9074232d392defc7be1806397ca80
Sergio Lucero <sergiolucero@gmail.com>
2022-07-05 03:22:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ import matplotlib.pyplot as plt
 
 data = datos_turisticos()
 
-st.header('Elementos Turísticos Quinta Región')
-st.pydeck_chart(LayeredDeck(HexTextLayers(data)))
-
-st.header('Estadísticas')
-Heatmap(data)
+FullDeck(data, 'Elementos Turísticos Quinta Región')
+Heatmap(data, 'Estadísticas')
 
 st.header('Datos')
 st.dataframe(data)
