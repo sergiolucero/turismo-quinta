5e33fb4140a9f00d3ddefed05d7b9ad7c919084f
Sergio Lucero <sergiolucero@gmail.com>
2022-07-05 21:34:13 -0400

    Update util.py

--- util.py
+++ util.py
@@ -1,8 +1,10 @@
 import sqlite3, pandas as pd
+import streamlit as st
 
 conn = sqlite3.connect('turismo2.db')
 sql = lambda q: pd.read_sql(q, conn)
 ##############################################
+@st.experimental_memo
 def datos_turisticos():
     df = sql('SELECT * FROM datos')
     df = df[[c for c in df.columns if c not in ('opening_hours')]]
